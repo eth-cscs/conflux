cbdd6aa1980e9a6cc1bc4316b73b76e1072ea0e6
Jens Eirik Saethre <jsaethre@daint104.login.cscs.ch>
2021-03-09 11:56:44 +0100

    first 32k measurement

--- launch/chol-fact-mkl.sh
+++ launch/chol-fact-mkl.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#SBATCH --job-name=cholesky-factorization-mkl-16k
+#SBATCH --job-name=cholesky-factorization-mkl-32k
 #SBATCH --time=00:50:00
-#SBATCH --nodes=8
-#SBATCH --output=cholesky-factorization-mkl-16k.txt
+#SBATCH --nodes=32
+#SBATCH --output=cholesky-factorization-mkl-32k.txt
 #SBATCH --constraint=mc
 #SBATCH --account=g34
 
 export OMP_NUM_THREADS=18
 
-srun -N 8 -n 16 ./build/examples/cholesky_scalapack --dim=16384 --tile=512 --grid=4x4 --run=0
+srun -N 32 -n 64 ./build/examples/cholesky_scalapack --dim=32768 --tile=512 --grid=8x8 --run=0
--- launch/chol-fact25d.sh
+++ launch/chol-fact25d.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#SBATCH --job-name=cholesky-factorization-65k
+#SBATCH --job-name=cholesky-factorization-32k
 #SBATCH --time=00:50:00
 #SBATCH --nodes=32
-#SBATCH --output=cholesky-factorization-65k.txt
+#SBATCH --output=cholesky-factorization-32k.txt
 #SBATCH --constraint=mc
 #SBATCH --account=g34
 
 export OMP_NUM_THREADS=18
 
-srun -N 32 -n 64 ./build/examples/cholesky_miniapp --dim=65536 --tile=512 --grid=4x4x4 --run=0
+srun -N 32 -n 64 ./build/examples/cholesky_miniapp --dim=32768 --tile=512 --grid=4x4x4 --run=0
