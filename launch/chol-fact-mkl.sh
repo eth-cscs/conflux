0915adf60254c7494acf3a44e2340226791be9ed
Jens Eirik Saethre <jsaethre@daint104.login.cscs.ch>
2021-03-09 12:19:14 +0100

    16k measurement

--- launch/chol-fact-mkl.sh
+++ launch/chol-fact-mkl.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#SBATCH --job-name=cholesky-factorization-mkl-32k
+#SBATCH --job-name=cholesky-factorization-mkl-16k
 #SBATCH --time=00:50:00
-#SBATCH --nodes=32
-#SBATCH --output=cholesky-factorization-mkl-32k.txt
+#SBATCH --nodes=18
+#SBATCH --output=cholesky-factorization-mkl-16k.txt
 #SBATCH --constraint=mc
 #SBATCH --account=g34
 
 export OMP_NUM_THREADS=18
 
-srun -N 32 -n 64 ./build/examples/cholesky_scalapack --dim=32768 --tile=512 --grid=8x8 --run=0
+srun -N 18 -n 36 ./build/examples/cholesky_scalapack --dim=16384 --tile=512 --grid=6x6 --run=0
