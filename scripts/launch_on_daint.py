cea7794f5f98b8708a59639246ba91bd60402091
Andre <gaillardandre@gmx.ch>
2021-03-11 14:54:59 +0100

    added cpu scripts

--- scripts/launch_on_daint.py
+++ scripts/launch_on_daint.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
 
     for launch_file in os.listdir(path):
         if "psychol" in launch_file or "scalapack" in launch_file:
+            os.system('source scripts/piz_daint_cpu.sh')
             os.system('chmod +x %s' %(path + launch_file))
             os.system("sbatch " + path + launch_file)
         
