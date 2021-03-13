8f8548c1514c8d6f0664c5cfb4ec20e2494fd640
saethrej <saethrej@student.ethz.ch>
2021-03-13 18:23:55 +0100

    removed Benchmark class, refactored cholesky_miniapp

--- examples/cholesky_miniapp.cpp
+++ examples/cholesky_miniapp.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2020, ETH Zurich
+ * Copyright (C) 2020-2021, ETH Zurich
  *
  * This product includes software developed at the Scalable Parallel Computing
  * Lab (SPCL) at ETH Zurich, headed by Prof. Torsten Hoefler. It was developped
@@ -21,198 +21,54 @@
  * You should have received a copy of the GNU General Public License along 
  * with this program. If not, see http://www.gnu.org/licenses.
 
- * @file main.cpp
+ * @file cholesky_miniapp.cpp
  * 
- * @brief main program for testing our Cholesky factorization program
+ * @brief miniapp for the psyCHOL algorithm, used for benchmarking
  * 
- * @authors Jens Eirik Saethre, Andre Gaillard, Pascal Müller, Marc Styger
- * Contact: {saethrej, andrega, stygerma, pamuelle}@student.ethz.ch
+ * @authors Jens Eirik Saethre, Andre Gaillard
+ * Contact: {saethrej, andrega}@.ethz.ch
  * 
- * @date 14.12.2020
+ * @date 13.03.2021
  */
-
 
 #include <string>
 #include <iostream>
 #include <sstream>
-
-#include <conflux/cholesky/CholeskyTypes.h>
-#include <conflux/cholesky/Cholesky.h>
-
+#include <fstream>
 #include <sys/types.h>
 #include <unistd.h>
-#include <getopt.h>
+#include <chrono>
 
 #include <mpi.h>
+#include <cxxopts.hpp>
 
-// only include Processor in debug or benchmarking mode
-#if defined(DEBUG) || defined(BENCHMARK)
-#include <conflux/cholesky/Processor.h>
-#include <sstream>
-#include <string>
-#endif
-
-// only include fstream, Processor and CholeskyProperties in DEBUG mode
-#ifdef DEBUG
-#include <fstream>
-#include <conflux/cholesky/CholeskyProperties.h>
-#endif // DEBUG
-
-
-// in debug or benchmarking mode, we require access to the global variable
-// proc defined in Cholesky.cpp
-#if defined(DEBUG) || defined(BENCHMARK)
-extern conflux::Processor *proc;
-#endif
-
-// in debug mode we require access to the global variables defined in Cholesky.cpp
-#ifdef DEBUG
-extern conflux::CholeskyProperties *prop;
-extern std::string pathToMatrix;
-#endif // DEBUG
-
-#define RUNS 5
+#include "conflux/cholesky/CholeskyTypes.h"
+#include "conflux/cholesky/Cholesky.h"
 
 /**
- * @brief prints help for command line arguments of this program
+ * @brief prints the timings to the desired stream
+ * @param out the desired stream (e.g. std::cout or a file)
+ * @param timings the vector containing the timings
+ * @param N the matrix dimension
+ * @param v the tile size
+ * @param grid the three-dimensional processor grid
  */
-void printHelp() 
+void printTimings(std::vector<double> &timings, std::ostream &out, int N, int v, conflux::ProcCoord grid[3])
 {
-    std::cerr << "Error: invalid parameters were provided. Allowed are: \n"
-              << "\t --help, -h: Display (this) help section. Write --help or -h\n"
-              << "\t --dim, -d: dimension of the matrix (>0). Write e.g. --dim=128 or -d 128 \n"
-              << "\t --tile, -v: (optional) tile size (>0). Write --tile=4 or -v 4 \n"
-              << "\t --grid, -g: (optional) processor grid. Write --grid=2x2x2 or -g 2x2x2\n"
-              << "\t --no-writeback, -x: (optional) don't write back result to file. Write --no-writeback or -x\n"
-              #if defined(BENCHMARK)
-              << "\t --run, -r: (optional, benchmarking) set current run. For benchmarking purposes.\n"
-              #endif
-              << "Try again.\n"
-              << std::endl;
-    return;
-}
-
-/**
- * @brief parses args from the command line that define a decomposition
- * 
- * @param argc the number of arguments from the shell
- * @param argv array with the arguments and their values
- * @param dim reference to the matrix dimension read in this function
- * @param tile reference to the tile size read in this funciton
- * @param grid pointer to the grid read in this function
- * @param store reference to the flag indicating whether or not to save output
- * @param run reference to the number of current run read in this function
- */
-#if defined(BENCHMARK)
-void parseArgs(int argc, char* argv[], uint32_t &dim, uint32_t &tile, 
-               conflux::ProcCoord *grid, bool &store, uint32_t &run)
-#else
-void parseArgs(int argc, char* argv[], uint32_t &dim, uint32_t &tile, 
-               conflux::ProcCoord *grid, bool &store)
-#endif
-{
-    // defines the allowed command line options in their "long" form
-    static const struct option long_args[] = {
-        {"dim", required_argument, 0, 'd'},
-        {"tile", required_argument, 0, 'v'},
-        {"grid", required_argument, 0, 'g'},
-        {"no-writeback", no_argument, 0, 'x'},
-        {"help", no_argument, 0, 'h'},
-        #if defined(BENCHMARK)
-        {"run", required_argument, 0, 'r'},
-        #endif
-        0
-    };
-
-    int index = -1;
-    int result;
-    struct option *opt = nullptr;
-
-    while (true) {
-        index = -1;
-        opt = nullptr;
-        #if defined(BENCHMARK)
-        result = getopt_long(argc, argv, "hd:v:g:xr:", long_args, &index);
-        #else
-        result = getopt_long(argc, argv, "hd:v:g:x", long_args, &index);
-        #endif
-
-        // break loop if all arguments were parsed
-        if (result == -1) break;
-
-        switch (result) {
-            case 'h': // help section
-                printHelp();
-                exit(0);
-
-            case 'd': // matrix dimensionality
-                try {
-                    dim = std::stoi(optarg);
-                    if (dim <= 0) {
-                        printHelp();
-                        exit(-1);
-                    }
-                } catch (std::exception e) {
-                    printHelp();
-                    exit(-1);
-                }
-                break;
-
-            case 'v': // tile size
-                try {
-                    tile = std::stoi(optarg);
-                    if (tile <= 0) {
-                        printHelp();
-                        exit(-1);
-                    }
-                } catch (std::exception e) {
-                    printHelp();
-                    exit(-1);
-                }
-                break;
-
-            case 'g': // processor grid
-                try {
-                    std::string gridStr = std::string(optarg);
-                    size_t x0 = gridStr.find('x', 0);
-                    size_t x1 = gridStr.find('x', x0+1);
-                    grid[0] = std::stoi(gridStr.substr(0, x0));
-                    grid[1] = std::stoi(gridStr.substr(x0+1, x1-x0-1));
-                    grid[2] = std::stoi(gridStr.substr(x1+1));
-                } catch (std::exception e) {
-                    printHelp();
-                    exit(-1);
-                }
-                if (grid[0] < 0 || grid[1] < 0 || grid[2] < 0) {
-                    printHelp();
-                    exit(-1);
-                }
-                break;
-
-            case 'x': // no write back (e.g. for benchmarking)
-                store = false;
-                break;
-
-            default: // fallback, indicates incorrect usage, print help and exit
-                printHelp();
-                exit(-1);
-
-            #if defined(BENCHMARK)
-            case 'r': // number of current run
-                try {
-                    run = std::stoi(optarg);
-                    if (run < 0) {
-                        printHelp();
-                        exit(-1);
-                    }
-                } catch (std::exception e) {
-                    printHelp();
-                    exit(-1);
-                }
-                break;
-            #endif
-        }
+    out << "==========================" << std::endl;
+    out << "    PROBLEM PARAMETERS:" << std::endl;
+    out << "==========================" << std::endl;
+    out << "Matrix size: " << N << std::endl;
+    out << "Tile size: " << v << std::endl;
+    out << "Processor grid: " << grid[0] << "x" << grid[1] << "x" << grid[2] << std::endl;
+    out << "Number of repetitions: " << timings.size() << std::endl;
+    out << "--------------------------" << std::endl;
+    out << "TIMINGS [ms] = ";
+    for (auto &time : timings) {
+        out << time << " ";
     }
+    out << std::endl;
+    out << "==========================" << std::endl;
 }
 
 /**
@@ -220,55 +76,94 @@ void parseArgs(int argc, char* argv[], uint32_t &dim, uint32_t &tile,
  * 
  * @param argc the number of command line arguments
  * @param argv an array of pointer to the command line arguments
+ * 
+ * @returns 0 on success, <0 otherwise
  */
 int main(int argc, char *argv[])
-{
-    // parse command line arguments
-    uint32_t matrixDim = 0;
-    uint32_t tileSize = 0;
-    conflux::ProcCoord grid[3] = {0, 0, 0};
-    bool storeMatrix = true;    
-    
-    #if defined(BENCHMARK)
-    uint32_t run = 0;
-    parseArgs(argc, argv, matrixDim, tileSize, grid, storeMatrix, run);
-    #else
-    parseArgs(argc, argv, matrixDim, tileSize, grid, storeMatrix);
-    uint32_t run = RUNS;
-    #endif
+{   
+    // set-up parser for the command line arguments
+    cxxopts::Options options(
+        "Cholesky Mini-App for Benchmarking",
+        "This is a miniapp computing A =LL^T for A s.p.d. and dim(A)= N*N"
+    );
+    options.add_options()
+        ("N,dim", "Dimension of input matrix", 
+        cxxopts::value<uint32_t>()->default_value("65536"))
+        ("v,tile", "Dimension of the tiles",
+        cxxopts::value<uint32_t>()->default_value("256"))
+        ("g,grid", "Processor grid to use for this factorization",
+        cxxopts::value<std::vector<uint32_t>>())
+        ("r,run", "The number of runs to perform",
+        cxxopts::value<uint32_t>()->default_value("5"))
+        ("h,help", "Print usage.");
 
-    // if matrix dimension is zero, we cannot proceed
-    if (matrixDim == 0) {
-        std::cerr << "Error: matrix dimension is a mandatory parameter. Try again." << std::endl;
-        exit(-1);
+    auto argv2 = const_cast<const char**>(argv);
+    auto result = options.parse(argc, argv2);
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        return -1;
+    }
+
+    // parse the command line arguments
+    uint32_t N = result["N"].as<uint32_t>();
+    uint32_t v = result["v"].as<uint32_t>();
+    uint32_t runs = result["r"].as<uint32_t>();
+    std::vector<uint32_t> tmpGrid = result["g"].as<std::vector<uint32_t>>();
+    conflux::ProcCoord grid[3] = {tmpGrid[0], tmpGrid[1], tmpGrid[2]};
+
+    // if matrix dimension is non-positive, we cannot proceed
+    if (N < 0) {
+        std::cerr << "Error: Invalid matrix dimension." << std::endl;
+        return -1;
     }
 
     // initialize the MPI environment
+    int rank;
     MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     // run the factorization once before benchmarking to make sure that the nodes
     // and threads per rank are already allocated when benchmarking
-    conflux::initialize(argc, argv, matrixDim, tileSize, grid);
+    conflux::initialize(argc, argv, N, v, grid);
     conflux::parallelCholesky();
     conflux::finalize(true);
 
-    // run the code N times
-    #ifndef DEBUG
-    for (size_t i = 0; i < RUNS; ++i) {
-        conflux::initialize(argc, argv, matrixDim, tileSize, grid);
+#ifndef DEBUG
+    // in non-debug mode, the benchmarking starts now after the warm-up was completed
+    std::vector<double> timings;
+    timings.reserve(runs);
+
+    // perform the cholesky factorization *runs* times and report the time
+    for (size_t i = 0; i < runs; ++i) {
+        // initialize the factorization from the ground for every iteration
+        conflux::initialize(argc, argv, N, v, grid);
         MPI_Barrier(MPI_COMM_WORLD);
-        #ifdef BENCHMARK
-        proc->benchmark->set_props("data/benchmarks/cholesky25d/output", i,
-            matrixDim, tileSize, grid[0], grid[1], grid[2]);
-        proc->benchmark->timer_start();
-        #endif
+
+        // this is the part of the code that will be benchmarked
+        auto start = std::chrono::high_resolution_clock::now();
         conflux::parallelCholesky();
-        #ifdef BENCHMARK
-        proc->benchmark->timer_stop();
-        #endif 
+        MPI_Barrier(MPI_COMM_WORLD);
+        auto end = std::chrono::high_resolution_clock::now();
+        double timeMs = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+
+        // finalize the library, and if successful, push timing back in the vector
         conflux::finalize(true);
+        timings.push_back(timeMs);
     }
-    #endif //debug
+
+    // let rank output the timing values
+    if (rank == 0) {
+        printTimings(timings, std::cout, N, v, grid);
+
+        // if you want to print the results to a file, uncomment the following:
+        //std::ofstream output("data/benchmarks/your-filename.txt", std::ios::out);
+        //printTimings(timings, output, N, v, grid);
+        //output.close();
+    }
+
+#endif // DEBUG
+
     MPI_Finalize();
+
     return 0;
 }
